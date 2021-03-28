385c610071003ea479c0e04ef3644d69e11b2df4
kopatych-ios <81485837+kopatych-ios@users.noreply.github.com>
2021-03-28 21:52:54 +0300

    Update script.js

--- project_diplom/script.js
+++ project_diplom/script.js
@@ -1,23 +1,61 @@
-$('.dropdown').click(function () {
-    $(this).attr('tabindex', 1).focus();
-    $(this).toggleClass('active');
-    $(this).find('.dropdown-menu').slideToggle(300);
-});
-$('.dropdown').focusout(function () {
-    $(this).removeClass('active');
-    $(this).find('.dropdown-menu').slideUp(300);
-});
-$('.dropdown .dropdown-menu li').click(function () {
-    $(this).parents('.dropdown').find('span').text($(this).text());
-    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
-});
-/*End Dropdown Menu*/
+var navigation = {
+    // Variables
+    $nav: document.querySelector('.nav'),
+    $navTrigger: document.querySelector('.nav__trigger'),
+    $navContent: document.querySelector('.nav__content'),
+    $navList: document.querySelector('.nav__list'),
+    transitionEnd: 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+    
+    init() {
+        var self = this;
 
+        // Handle the transitions
+        self.$navTrigger.addEventListener('click', function() {
+            if (!self.$navTrigger.classList.contains('is-active')) {
+                // .nav--trigger active
+                self.$navTrigger.classList.add('is-active');
 
-$('.dropdown-menu li').click(function () {
-var input = '<strong>' + $(this).parents('.dropdown').find('input').val() + '</strong>',
-  msg = '<span class="msg">Hidden input value: ';
-$('.msg').html(msg + input + '</span>');
-}); 
+                // .nav active
+                if (!self.$nav.classList.contains('is-active')) {
+                    self.$nav.classList.add('is-active');
+                    self.$nav.addEventListener('transitionend', function(e) {
+                        if (e.propertyName == 'width' && self.$navTrigger.classList.contains('is-active')) {
+                            // .nav__content active
+                            self.$navContent.classList.add('is-active');
+                        }
+                    });
+                } else {
+                    self.$navContent.classList.add('is-active');                    
+                }
 
-console.log("Dct jr)")
+                // no-csstransitions fallback
+                if (document.documentElement.classList.contains('no-csstransitions')) {
+                    self.$navContent.classList.add('is-active');
+                }
+            } else {
+                // .nav--trigger inactive
+                self.$navTrigger.classList.remove('is-active');
+                
+                // .nav__content inactive
+                if (self.$navContent.classList.contains('is-active')) {
+                    self.$navContent.classList.remove('is-active');
+                    self.$navContent.addEventListener('transitionend', function(e) {    
+                        if (e.propertyName == 'opacity' && !self.$navTrigger.classList.contains('is-active')) {
+                            // .nav inactive
+                            self.$nav.classList.remove('is-active');                        
+                        }
+                    });                    
+                } else {
+                    self.$nav.classList.remove('is-active');                    
+                }
+
+                // no-csstransitions fallback
+                if (document.documentElement.classList.contains('no-csstransitions')) {
+                    self.$nav.classList.remove('is-active');
+                }
+            }
+        });
+    }
+}
+
+navigation.init();
